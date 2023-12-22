dd62005899a320c3d051f9bcd6fa403791c5fc56
David <d.stefanov16@icloud.com>
2023-12-22 11:38:11 +0100

    ersterTest_movie_view.get_info()

--- library/tests/MovieListView_test.py
+++ library/tests/MovieListView_test.py
@@ -1,1 +1,16 @@
 import pytest
+from django.urls import reverse
+from django.test import RequestFactory
+# Hier MovieView importieren !
+
+#Test of Data from Movie can be outputed
+def test_if_data_of_movie_is_outputed_correctly():
+    #Arrange
+    test_object = MovieView()
+    request = RequestFactory().get(reverse('movie-info'))
+    #Act
+    response = movie_view.get_info(request)
+    
+    #Assert
+    assert response.status_code = 200
+    assert 'Expected Data' in response.content.decode('utf-8')
