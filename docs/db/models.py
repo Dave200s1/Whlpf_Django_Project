1fcfe373ce887e2f02de71ee206b877c8f953f89
Andrej Schreiner <a.schreiner@telic.de>
2024-01-19 13:01:01 +0100

    add regie to model; update urls; add task distributions

--- docs/db/models.py
+++ docs/db/models.py
@@ -53,6 +53,12 @@ class Autor(models.Model):
     def __str__(self):
         return self.name
 
+class Regie(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
 class UserBorrowed(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     media = models.ForeignKey(Media, on_delete=models.CASCADE)
