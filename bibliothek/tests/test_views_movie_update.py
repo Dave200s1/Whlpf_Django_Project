842abca2bedfb41ddd8674ed33f7e159b24674ec
David <d.stefanov16@icloud.com>
2024-01-26 11:02:45 +0100

    Movie_Vies_Update_test_methoden

--- bibliothek/tests/test_views_movie_update.py
+++ bibliothek/tests/test_views_movie_update.py
@@ -1,6 +1,20 @@
 from  django.test import TestCase
+
+from django.utils import timezone
+
 from MovieUpdateView import MovieUpdateViewClass
 
-class MovieUpdateListTest(TestCase):
+class MovieUpdateViewTest(TestCase):
     
     @classmethod
+    def test_update_form_date_field_label(self):
+        pass
+    
+    def test_update_form_date_too_far_in_future(self):
+        pass
+    
+    def test_update_form_date_today(self):
+        pass
+    
+    def test_update_form_date_in_past(self):
+        pass
