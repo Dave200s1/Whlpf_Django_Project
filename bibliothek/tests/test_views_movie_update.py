35f24a860006432b50a9621b30958109b62920bd
David <d.stefanov16@icloud.com>
2024-02-09 10:48:52 +0100

    MovieViewUpdate_Post_Get

--- bibliothek/tests/test_views_movie_update.py
+++ bibliothek/tests/test_views_movie_update.py
@@ -2,9 +2,13 @@ from  django.test import TestCase
 
 from django.utils import timezone
 
-from MovieUpdateView import MovieUpdateViewClass
+from MovieUpdateViewClass import MovieUpdateViewClass
 
 class MovieUpdateViewTest(TestCase):
+    
+    def setUp(self) -> None:
+        self.movie = Movie.objects.create(title= "Test Movie", director = "Michael Bay")
+        
     
     @classmethod
     #expired date
@@ -17,12 +21,21 @@ class MovieUpdateViewTest(TestCase):
         #Assert
         self.assertFalse(result)
         
+    # Test the get method of the MovieUpdateView
+    def test_get_method(self):
+        response = self.client.get(reverse('movie-update', kwargs={'pk': self.movie.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'movie_update.html')
     
-    def test_update_form_date_too_far_in_future(self):
-        pass
+    # Test the post method of the MovieUpdateView
+    def test_post_method(self):
+        updated_title = 'Updated Movie'
+        updated_director = 'Updated Director'
+        response = self.client.post(reverse('movie-update', kwargs={'pk': self.movie.pk}), {'title': updated_title, 'director': updated_director})
+        self.assertEqual(response.status_code, 302)  # 302 is the status code for a successful redirect
+        self.movie.refresh_from_db()  # Refresh the movie instance from the database
+        self.assertEqual(self.movie.title, updated_title)
+        self.assertEqual(self.movie.director, updated_director)
+        
     
-    def test_update_form_date_today(self):
-        pass
-    
-    def test_update_form_date_in_past(self):
-        pass
+   
