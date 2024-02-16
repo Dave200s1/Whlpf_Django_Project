20345b0af6403e03c8ab2977a5275f992f1f2b6c
David <d.stefanov16@icloud.com>
2024-02-16 10:56:44 +0100

    models_regestry

--- bibliothek/lib_app/admin.py
+++ bibliothek/lib_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import Media
+from .models import Film
+from .models import Author
+from .models import Regie
+from .models import Book
+from .models import Genre
+from .models import Language
+from .models import CustomUser
+from .models import UserBorrowed
+from .models import UserReserved
+from .models import UserReview
 # Register your models here.
+
+admin.site.register(Film)
+admin.site.register(BaseException)
+admin.site.register(Media)
+admin.site.register(Author)
+admin.site.register(Regie)
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(CustomUser)
+admin.site.register(UserBorrowed)
+admin.site.register(UserReview)
+
