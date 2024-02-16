907eb86fea6262a95cf6f90151b93ffce705a8d2
Andrej Schreiner <a.schreiner@telic.de>
2024-02-16 10:40:36 +0100

    add gitignore

--- bibliothek/lib_app/models.py
+++ bibliothek/lib_app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 MEDIA_CHOICES = [('book', 'Book'), ('film', 'Film')]
 
+
 class Media(models.Model):
     title = models.CharField(max_length=200)
     media_type = models.CharField(max_length=20, choices=MEDIA_CHOICES)
