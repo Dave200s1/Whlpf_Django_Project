136c84d23816c722ed33adc1d69ef602f7b8d250
Andrej Schreiner <a.schreiner@telic.de>
2024-02-16 10:09:00 +0100

    add models

--- bibliothek/lib_app/models.py
+++ bibliothek/lib_app/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 MEDIA_CHOICES = [('book', 'Book'), ('film', 'Film')]
 
-
 class Media(models.Model):
     title = models.CharField(max_length=200)
     media_type = models.CharField(max_length=20, choices=MEDIA_CHOICES)
