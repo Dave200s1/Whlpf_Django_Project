1eae0f97758f77d3d7efc958ed1479376d446e2b
Andrej Schreiner <a.schreiner@telic.de>
2024-03-15 11:27:23 +0100

    add admin content fields, filters und search

--- bibliothek/lib_app/admin.py
+++ bibliothek/lib_app/admin.py
@@ -1,17 +1,31 @@
 from django.contrib import admin
 from .models import Movie, Book, Author, Director, Genre, Language, UserBorrowed, UserReserved, UserReview, User
 
-admin.site.register(Book)
-admin.site.register(Movie)
 admin.site.register(Author)
 admin.site.register(Director)
 admin.site.register(Genre)
 admin.site.register(Language)
-admin.site.register(UserBorrowed)
-admin.site.register(UserReserved)
-admin.site.register(UserReview)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "genre", "author", "language", "ISBN", "release_year", "borrowed", "reserved"]
+    list_filter = ["borrowed", "reserved", "genre", "author", "language"]
+    search_fields = ["title"]
+
+admin.site.register(Book, BookAdmin)
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["title", "genre", "director", "language", "release_year", "borrowed", "reserved"]
+    list_filter = ["borrowed", "reserved", "genre", "director", "language"]
+    search_fields = ["title"]
+
+admin.site.register(Movie, MovieAdmin)
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "last_name", "first_name", "locked"]
-
+    list_filter = ["locked"]
+    search_fields = ["username", "last_name"]
 admin.site.register(User, UserAdmin)
+
+admin.site.register(UserBorrowed)
+admin.site.register(UserReserved)
+admin.site.register(UserReview)
