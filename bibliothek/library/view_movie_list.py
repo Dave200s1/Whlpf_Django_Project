6991373657555a15a4cc8cb923b0152bd5be53a0
David <d.stefanov16@icloud.com>
2024-04-16 16:22:32 +0200

    list_items_day_month_year

--- bibliothek/library/view_movie_list.py
+++ bibliothek/library/view_movie_list.py
@@ -1,16 +1,20 @@
 import sqlite3
 
-
-
 class MovieListView:
+
+    def __init__(self, db_file):
+        self.db_file = db_file
+
+    def get_connection(self):
+        return sqlite3.connect(self.db_file)
+
     def get_movies(self):
-        conn = sqlite3.connect('Artikel.db')  
+        conn = self.get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT Name, Genre FROM Artikel")
         movies = [{"title": row[0], "genre": row[1]} for row in cursor.fetchall()]
         conn.close()
         return movies
-        
 
     def sort_alphabetically(self, movies):
         sorted_movies = sorted(movies, key=lambda x: x["title"])
@@ -19,3 +23,35 @@ class MovieListView:
     def sort_genre(self, movies, selected_genre):
         sorted_movies = [movie for movie in movies if movie["genre"] == selected_genre]
         return sorted_movies
+
+    def extract_day_from_database(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT ErscheinungsDatum FROM Artikel")
+        date_string = cursor.fetchone()[0]
+        day_substring = date_string[:2]
+        conn.close()
+        return day_substring
+
+    def extract_month_from_database(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT ErscheinungsDatum FROM Artikel")
+        date_string = cursor.fetchone()[0]
+        month_substring = date_string[3:5]
+        conn.close()
+        return month_substring
+
+    def extract_year_from_database(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT ErscheinungsDatum FROM Artikel")
+        date_string = cursor.fetchone()[0]
+        year_substring = date_string[6:]
+        conn.close()
+        return year_substring
+
+# Test code
+if __name__ == "__main__":
+    movie_list_view = MovieListView('Artikel.db')
+    print(movie_list_view.extract_month_from_database())
